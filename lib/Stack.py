2ad365a92568e697e3bd566509f374a55a06a42b
Nathan Mwai <nathan.mwai@student.moringaschool.com>
2024-09-11 16:16:06 +0300

    done and optimized

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,34 @@
 class Stack:
 
     def __init__(self, items = [], limit = 100):
+        self.items = items
+        self.limit = limit
         pass
 
     def isEmpty(self):
-        pass
+        return len(self.items) == 0
+        
+    def full(self):
+        return len(self.items) == self.limit
+
 
     def push(self, item):
-        pass
+        self.items.append(item) if not self.full() else None
+
 
     def pop(self):
-        pass
+        return self.items.pop() if not self.isEmpty() else None
 
     def peek(self):
-        pass
+        return self.items[-1] if not self.isEmpty() else None
     
     def size(self):
-        pass
-
-    def full(self):
-        pass
-
+        return len(self.items)
+        
     def search(self, target):
-        pass
+        reverse_items = self.items[::-1]
+        for num in range(len(reverse_items)):
+            if reverse_items[num] == target:
+                return num
+        return -1
+            
